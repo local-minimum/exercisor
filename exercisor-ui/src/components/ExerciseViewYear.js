20f170ce82108c677eb177c88b5403ad6f9a07ad
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-01-03 18:54:45 +0100

    tweaking filtering

--- exercisor-ui/src/components/ExerciseViewYear.js
+++ exercisor-ui/src/components/ExerciseViewYear.js
@@ -6,6 +6,7 @@ import { filterEvents } from '../util';
 export default function ExerciseViewYear(props) {
   const locArr = props.location.pathname.split('/');
   const year = locArr[locArr.indexOf(props.match.params.name) + 1];
+  console.log(locArr, year, props);
   const events = filterEvents(props.events, year);
   return (
     <div>
