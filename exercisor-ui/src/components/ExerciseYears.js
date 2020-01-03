468ccb7743bcfe4ce644e85f5f207b1898773cec
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-01-03 19:13:31 +0100

    total added to nav

--- exercisor-ui/src/components/ExerciseYears.js
+++ exercisor-ui/src/components/ExerciseYears.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default function ExerciseYears({ years, match }) {
     const path = match.url.split('/')
     const prefix = path.slice(0, path.indexOf(match.params.name) + 1).join('/')
+    let total = Object.values(years).reduce((tot, count) => tot + count, 0);
     const yearsSpans = Object
       .entries(years)
       .map(([year, count]) => (
@@ -13,7 +14,8 @@ export default function ExerciseYears({ years, match }) {
           </span>
         </Link>
       ));
+    const backLink = <Link to={prefix}><span className="year-summary"><strong>Totalt</strong> ({total})</span></Link>;
     return (
-      <div className="years-nav">{yearsSpans.length === 0 ? <em>Inget pass rapporterat än</em> : yearsSpans}</div>
+      <div className="years-nav">{yearsSpans.length === 0 ? <em>Inget pass rapporterat än</em> : yearsSpans}{backLink}</div>
     );
 }
