711dd3fc34ff0e779bc0cadb9904bdc20aff5ed1
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-01-03 18:57:12 +0100

    making year number

--- exercisor-ui/src/components/ExerciseViewYear.js
+++ exercisor-ui/src/components/ExerciseViewYear.js
@@ -6,8 +6,7 @@ import { filterEvents } from '../util';
 export default function ExerciseViewYear(props) {
   const locArr = props.location.pathname.split('/');
   const year = locArr[locArr.indexOf(props.match.params.name) + 1];
-  console.log(locArr, year, props);
-  const events = filterEvents(props.events, year);
+  const events = filterEvents(props.events, year != null ? Number(year) : null);
   return (
     <div>
       <ExerciseSummary events={events} year={year} />
